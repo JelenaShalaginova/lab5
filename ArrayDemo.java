4dde7a61c26b0c5ff5f09becbf34fdf6ca0c7553
Jelena Shalaginova <jelena.shalaginova@gmail.com>
2020-04-14 16:31:16 +0300

    first commit

---
+++ ArrayDemo.java
@@ -0,0 +1,28 @@
+package lab5;
+
+public class ArrayDemo {
+
+	//declare data members
+	private int nums[][];
+
+	//constructor
+	public ArrayDemo() {
+	}
+
+	public int[][] getNums() {
+		return nums;
+	}
+
+	public void setNums(int[][] nums) {
+		this.nums = nums;
+	}
+
+	public void setArrayNumber(int row, int column, int num) {
+		nums[row][column] = num;
+	}
+	
+	
+	
+	
+	
+}
